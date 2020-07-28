d800df7f36a146a375ae19830cf5ba237128a239
adolfoquende <adolfoquende@hotmail.com>
2020-07-28 18:13:30 +0100

    Adicionando Single Threads

--- chat/src/ChatServer/ServerMain.java
+++ chat/src/ChatServer/ServerMain.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  *
@@ -25,11 +26,7 @@ public class ServerMain {
            
            Socket clientSocket=serverSocket.accept();
            System.out.println("Accepted connection from: "+clientSocket);
-           OutputStream outputStream= clientSocket.getOutputStream();
-           outputStream.write("Hello GQ29\n".getBytes());
-           clientSocket.close();
-           
-           
+           handleClientSocket(clientSocket);   
        }
        
        
@@ -37,8 +34,26 @@ public class ServerMain {
    }  catch(IOException erro){
        
        erro.printStackTrace();
+   }catch(InterruptedException erro){
+       
+       erro.printStackTrace();
    }
         
     }
     
+    //Metodo responsavel por criar varias conexoes
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException         
+    {
+        OutputStream outputStream= clientSocket.getOutputStream();
+   for(int i=0; i<10; i++)
+   {
+   
+         outputStream.write(("Hello GQ29 "+new Date()+"\n").getBytes());
+         Thread.sleep(1000);
+   }
+              
+     clientSocket.close();   
+        
+    }
+            
 }
